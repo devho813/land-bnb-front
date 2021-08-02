2ecdb86ae7c5992b73dea82aaf2854f5febdb6b2
devho813 <devho813@gmail.com>
2021-08-02 22:53:55 +0900

    add : emotion 세팅, svg 컴포넌트 설정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
+import { css } from "@emotion/react";
 import React from "react";
 
 function Home() {
-  return <div>hello world</div>;
+  return <div css={container}>hello world</div>;
 }
 
 export default Home;
+
+const container = css`
+  font-size: 21px;
+  color: gray;
+`;
