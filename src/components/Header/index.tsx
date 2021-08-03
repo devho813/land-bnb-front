8a39df05f432a95e42f12ff588e4af720332c055
devho813 <devho813@gmail.com>
2021-08-03 21:59:56 +0900

    add : 헤더 로그인, 회원가입 스타일링

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,13 @@
 import AirbnbLogoIcon from "../../../public/assets/logo.svg";
 import AirbnbLogoTextIcon from "../../../public/assets/logo_text.svg";
-import { container, logo, logoWrapper } from "./styles";
+import {
+  authButtons,
+  container,
+  loginButton,
+  logo,
+  logoWrapper,
+  signUpButton,
+} from "./styles";
 import Link from "next/link";
 
 function Header() {
@@ -12,6 +19,14 @@ function Header() {
           <AirbnbLogoTextIcon />
         </div>
       </Link>
+      <div css={authButtons}>
+        <button type="button" css={signUpButton}>
+          회원가입
+        </button>
+        <button type="button" css={loginButton}>
+          로그인
+        </button> 
+      </div>
     </header>
   );
 }
--- src/components/Header/styles.tsx
+++ src/components/Header/styles.tsx
@@ -1,4 +1,5 @@
 import { css } from "@emotion/react";
+import palette from "../../styles/palette";
 
 export const container = css`
   position: sticky;
@@ -19,4 +20,34 @@ export const logoWrapper = css`
 `;
 export const logo = css`
   margin-right: 6px;
+`;
+
+export const authButtons = css``;
+export const signUpButton = css`
+  height: 42px;
+  margin-right: 8px;
+  padding: 0 16px;
+  border: 0;
+  border-radius: 21px;
+  background-color: white;
+  cursor: pointer;
+  outline: none;
+
+  &:hover {
+    background-color: ${palette.gray_f7};
+  }
+`;
+export const loginButton = css`
+  height: 42px;
+  padding: 0 16px;
+  border: 0;
+  box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.18);
+  border-radius: 21px;
+  background-color: white;
+  cursor: pointer;
+  outline: none;
+
+  &:hover {
+    box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.12);
+  }
 `;
