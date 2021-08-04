955422ef91bbca6d1e45282ed28021de97e8cd17
devho813 <devho813@gmail.com>
2021-08-04 18:37:44 +0900

    add : 회원가입 폼 마크업

--- src/components/SignUpModal/index.tsx
+++ src/components/SignUpModal/index.tsx
@@ -1,7 +1,32 @@
-import { SignUpModalContainer } from "./styles";
+import { inputWrapper, modalCloseXIcon, SignUpModalContainer } from "./styles";
+import CloseXIcon from "../../../public/assets/modal_close_x_icon.svg";
+import MailIcon from "../../../public/assets/mail.svg";
+import PersonIcon from "../../../public/assets/person.svg";
+import OpenedEyeIcon from "../../../public/assets/opened_eye.svg";
+import ClosedEyeIcon from "../../../public/assets/closed_eye.svg"
 
 function SignUpModal() {
-  return <div css={SignUpModalContainer}>Sign Up</div>;
+  return (
+    <div css={SignUpModalContainer}>
+      <CloseXIcon css={modalCloseXIcon} />
+      <div css={inputWrapper}>
+        <input type="email" name="email" placeholder="이메일 주소" />
+        <MailIcon />
+      </div>
+      <div css={inputWrapper}>
+        <input type="text" placeholder="이름(예: 길동)" />
+        <PersonIcon />
+      </div>
+      <div css={inputWrapper}>
+        <input type="text" placeholder="성(예: 홍)" />
+        <PersonIcon />
+      </div>
+      <div css={inputWrapper}>
+        <input type="password" placeholder="비밀번호 설정하기" />
+        <OpenedEyeIcon />
+      </div>
+    </div>
+  );
 }
 
 export default SignUpModal;
--- src/components/SignUpModal/styles.tsx
+++ src/components/SignUpModal/styles.tsx
@@ -1,8 +1,40 @@
 import { css } from "@emotion/react";
+import palette from "../../styles/palette";
 
 export const SignUpModalContainer = css`
   width: 568px;
-  height: 614px;
+  padding: 32px;
   background-color: white;
   z-index: 11;
+`;
+
+export const modalCloseXIcon = css`
+  cursor: pointer;
+  display: block;
+  margin: 0 0 40px auto;
+`;
+
+export const inputWrapper = css`
+  position: relative;
+  margin-bottom: 16px;
+  input {
+    position: relative;
+    width: 100%;
+    height: 46px;
+    padding: 0 44px 0 11px;
+    border: 1px solid ${palette.gray_eb};
+    border-radius: 4px;
+    font-size: 16px;
+    outline: none;
+
+    &::placeholder {
+      color: ${palette.gray_76}
+    }
+  }
+
+  svg {
+    position: absolute;
+    right: 11px;
+    top: 16px;
+  }
 `;
